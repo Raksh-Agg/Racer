c688c0aa6188e3acd24ec1a54014cb73e224f87b
Rakshit Aggarwal <rakshit.agg04@gmail.com>
2023-07-13 23:06:38 +0530

    Single Row

--- move.js
+++ move.js
@@ -2,18 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const squares = Array.from(document.getElementsByClassName('square'));
     let currentSquareIndex = 0;
 
-    // Initial coloring of the first square
     squares[currentSquareIndex].classList.add('selected');
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'ArrowRight') {
-            squares[currentSquareIndex].classList.remove('selectedSquare');
-            // squares[currentSquareIndex].classList.add('square');
+            squares[currentSquareIndex].classList.remove('selectedSqaure');
+            squares[currentSquareIndex].classList.add('square');
 
             currentSquareIndex = (currentSquareIndex + 1) % squares.length;
 
-            // squares[currentSquareIndex].classList.remove('square');
-            squares[currentSquareIndex].classList.add('selectedSquare');
+            squares[currentSquareIndex].classList.remove('square');
+            squares[currentSquareIndex].classList.add('selectedSqaure');
         }
     });
 });
