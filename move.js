2be237013055e31c10e9267f443a56dd29446d5d
Rakshit Aggarwal <rakshit.agg04@gmail.com>
2023-07-15 02:29:13 +0530

    Temporary Commit

--- move.js
+++ move.js
@@ -1,18 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const squares = Array.from(document.getElementsByClassName('square'));
+    // const squares = Array.from(document.getElementsByClassName('square'));
+    let Row_Top = document.getElementById("row_top").children;
+    // for(var i = 0; i < searchEles.length; i++) {
+    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
+    //         if(searchEles[i].id.indexOf('RT') == 0) {
+    //             Row_Top.push(searchEles[i]);
+    //         }
+    //     }
+    // }
+    let Row_Bottom = document.getElementById("row_bottom").children;
+    // for(var i = 0; i < searchEles.length; i++) {
+    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
+    //         if(searchEles[i].id.indexOf('RB') == 0) {
+    //             Row_Bottom.push(searchEles[i]);
+    //         }
+    //     }
+    // }
+    let Column_Right = document.getElementById("column_right").children;
+    // for(var i = 0; i < searchEles.length; i++) {
+    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
+    //         if(searchEles[i].id.indexOf('CR') == 0) {
+    //             Column_Right.push(searchEles[i]);
+    //         }
+    //     }
+    // }
+    let Column_Left = document.getElementById("column_left").children;
+    // for(var i = 0; i < searchEles.length; i++) {
+    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
+    //         if(searchEles[i].id.indexOf('CL') == 0) {
+    //             Column_Left.push(searchEles[i]);
+    //         }
+    //     }
+    // }
     let currentSquareIndex = 0;
 
-    squares[currentSquareIndex].classList.add('selected');
+    Row_Top[currentSquareIndex].classList.add('selectedSquare');
+    Row_Top[currentSquareIndex].classList.remove('square');
+    // print(Row_Top)
+    // squares[currentSquareIndex].classList.add('selected');
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'ArrowRight') {
-            squares[currentSquareIndex].classList.remove('selectedSqaure');
-            squares[currentSquareIndex].classList.add('square');
+            Row_Top[currentSquareIndex].classList.remove('selectedSqaure');
+            Row_Top[currentSquareIndex].classList.add('square');
 
-            currentSquareIndex = (currentSquareIndex + 1) % squares.length;
+            currentSquareIndex = (currentSquareIndex + 1) % Row_Top.length;
 
-            squares[currentSquareIndex].classList.remove('square');
-            squares[currentSquareIndex].classList.add('selectedSqaure');
+            Row_Top[currentSquareIndex].classList.remove('square');
+            Row_Top[currentSquareIndex].classList.add('selectedSqaure');
         }
     });
 });
