c62985c9d2da208473e319fa98c7cc49845c34db
Rakshit Aggarwal <rakshit.agg04@gmail.com>
2023-07-15 12:48:52 +0530

    Functional Keys

--- move.js
+++ move.js
@@ -1,53 +1,55 @@
+const horizontalLength = 13;
+const verticalLength = 3;
+const raceLength = 2*horizontalLength + 2*verticalLength;
+function switchSelected (Race, currentIndex, direction)
+{
+    Race[currentIndex].classList.remove('selectedSquare');
+    Race[currentIndex].classList.add('square');
+    currentIndex = (currentIndex + direction + raceLength) % raceLength;
+    Race[currentIndex].classList.remove('square');
+    Race[currentIndex].classList.add('selectedSquare');
+}
 document.addEventListener('DOMContentLoaded', function() {
-    // const squares = Array.from(document.getElementsByClassName('square'));
-    const Row_Top = Array.from(document.getElementById("row_top").children);
-    // for(var i = 0; i < searchEles.length; i++) {
-    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
-    //         if(searchEles[i].id.indexOf('RT') == 0) {
-    //             Row_Top.push(searchEles[i]);
-    //         }
-    //     }
-    // }
-    const Row_Bottom = Array.from(document.getElementById("row_bottom").children);
-    // for(var i = 0; i < searchEles.length; i++) {
-    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
-    //         if(searchEles[i].id.indexOf('RB') == 0) {
-    //             Row_Bottom.push(searchEles[i]);
-    //         }
-    //     }
-    // }
-    const Column_Right = Array.from(document.getElementById("column_right").children);
-    // for(var i = 0; i < searchEles.length; i++) {
-    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
-    //         if(searchEles[i].id.indexOf('CR') == 0) {
-    //             Column_Right.push(searchEles[i]);
-    //         }
-    //     }
-    // }
-    const Column_Left = Array.from(document.getElementById("column_left").children);
-    // for(var i = 0; i < searchEles.length; i++) {
-    //     if(searchEles[i].tagName == 'SELECT' || searchEles.tagName == 'INPUT') {
-    //         if(searchEles[i].id.indexOf('CL') == 0) {
-    //             Column_Left.push(searchEles[i]);
-    //         }
-    //     }
-    // }
-    let currentSquareIndex = 0;
-
-    // Row_Top[currentSquareIndex].classList.add('selectedSquare');
-    // Row_Top[currentSquareIndex].classList.remove('square');
-    // print(Row_Top)
-    // squares[currentSquareIndex].classList.add('selected');
-
+    const Row_Top = Array.from(document.querySelectorAll('.row_top > .square'));
+    const Row_Bottom = Array.from(document.querySelectorAll('.row_bottom > .square'));
+    const Column_Right = Array.from(document.querySelectorAll('.column_right > .square'));
+    const Column_Left = Array.from(document.querySelectorAll('.column_left > .square'));
+    const Race = Row_Top
+    Race.push.apply(Race, Column_Right)
+    Row_Bottom.reverse();
+    Column_Left.reverse();
+    Race.push.apply(Race, Row_Bottom);
+    Race.push.apply(Race, Column_Left);
+    let currentIndex = 0;
+    Race[currentIndex].classList.remove('square');
+    Race[currentIndex].classList.add('selectedSquare');
+    const rowTopEnd = horizontalLength-1;
+    const columnRightEnd = rowTopEnd + verticalLength+1;
+    const rowBottomEnd = columnRightEnd + horizontalLength-1;
+    const columnLeftEnd = rowBottomEnd + verticalLength+1;
     document.addEventListener('keydown', function(event) {
-        if (event.key === 'ArrowRight') {
-            Row_Top[currentSquareIndex].classList.remove('selectedSqaure');
-            Row_Top[currentSquareIndex].classList.add('square');
-
-            currentSquareIndex = (currentSquareIndex + 1) % Row_Top.length;
-
-            Row_Top[currentSquareIndex].classList.remove('square');
-            Row_Top[currentSquareIndex].classList.add('selectedSqaure');
-        }
+        if (event.key === 'ArrowDown' && currentIndex == 0)
+            switchSelected(Race, currentIndex--, -1);
+        if (event.key === 'ArrowRight' && currentIndex < rowTopEnd)
+            switchSelected(Race, currentIndex++, 1);
+        if (event.key === 'ArrowLeft' && currentIndex <= rowTopEnd && currentIndex > 0)
+            switchSelected(Race, currentIndex--, -1);
+        if (event.key === 'ArrowDown' && currentIndex >= rowTopEnd && currentIndex < columnRightEnd)
+            switchSelected(Race, currentIndex++, 1);
+        if (event.key === 'ArrowUp' && currentIndex > rowTopEnd && currentIndex <= columnRightEnd)
+            switchSelected(Race, currentIndex--, -1);
+        if (event.key === 'ArrowLeft' && currentIndex >= columnRightEnd && currentIndex < rowBottomEnd)
+            switchSelected(Race, currentIndex++, 1);
+        if (event.key === 'ArrowRight' && currentIndex > columnRightEnd && currentIndex <= rowBottomEnd)
+            switchSelected(Race, currentIndex--, -1);
+        if (event.key === 'ArrowUp' && currentIndex >= rowBottomEnd && currentIndex < columnLeftEnd)
+            switchSelected(Race, currentIndex++, 1);
+        if (event.key === 'ArrowDown' && currentIndex > rowBottomEnd && currentIndex <= columnLeftEnd)
+            switchSelected(Race, currentIndex--, -1);
+        if (event.key === 'K' || event.key === 'k')
+            switchSelected(Race, currentIndex++, 1);
+        currentIndex = (currentIndex + raceLength) % raceLength;
+        
+        console.log(columnLeftEnd);
     });
 });
